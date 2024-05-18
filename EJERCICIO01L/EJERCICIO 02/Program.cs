5b5f23dea9a7d60c55a878521d755021b391d4cb
GabrielaCode2104 <167550549+GabrielaCode2104@users.noreply.github.com>
2024-05-18 03:34:08 -0500

    ejer2

---
+++ EJERCICIO01L/EJERCICIO 02/Program.cs
@@ -0,0 +1,18 @@
+﻿//2.Escriba una función que reciba un conjunto de palabras y devuelva un conjunto con las palabras que comienzan con una letra determinada. 
+
+class Program
+{
+    static void Main()
+    {
+        string[] palabras = {"manzana", "plátano", "kiwi", "mango", "naranja", "pera", "uva" };
+        char letra = 'p';
+        var empezarConLetra = GetWordsStartingWithLetter(palabras, letra);
+
+        Console.WriteLine(string.Join(" ", empezarConLetra));
+    }
+
+    static IEnumerable<string> GetWordsStartingWithLetter(IEnumerable<string> palabras, char letra)
+    {
+        return palabras.Where(w => w[0] == letra);
+    }
+}
