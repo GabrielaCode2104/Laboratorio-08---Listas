2e5a923ce3556e7cd98dd76c525db902176b7788
GabrielaCode2104 <167550549+GabrielaCode2104@users.noreply.github.com>
2024-05-18 15:27:44 -0500

    14

---
+++ EJERCICIO01L/Ejercicio 14/Program.cs
@@ -0,0 +1,18 @@
+﻿// #14
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese los números separados por comas: ");
+        string input = Console.ReadLine();
+        List<int> numer = input.Split(',').Select(x => int.Parse(x.Trim())).ToList();
+        List<int> numerosNoDuplicados = FindnumerosNoDuplicados(numer);
+
+        Console.WriteLine("Números no duplicados: " + string.Join(", ", numerosNoDuplicados));
+    }
+
+    static List<int> FindnumerosNoDuplicados(List<int> numer)
+    {
+        return numer.GroupBy(n => n).Where(g => g.Count() == 1).Select(g => g.Key).ToList();
+    }
+}
