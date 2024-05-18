3baefc1661811889cbde1bd172d2659bc4f1ef6c
GabrielaCode2104 <167550549+GabrielaCode2104@users.noreply.github.com>
2024-05-18 11:52:48 -0500

    10

---
+++ EJERCICIO01L/Ejercicio 10/Program.cs
@@ -0,0 +1,20 @@
+﻿// #10
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese las palabras separadas por comas: ");
+        string input = Console.ReadLine();
+        List<string> palabras = input.Split(',').Select(x => x.Trim()).ToList();
+        Console.WriteLine("Ingrese la letra deseada: ");
+        char letra = Convert.ToChar(Console.ReadLine());
+        List<string> palabrasConLetra = FindpalabrasConLetra(palabras, letra);
+
+        Console.WriteLine("Palabras que contienen la letra " + letra + ": " + string.Join(", ", palabrasConLetra));
+    }
+
+    static List<string> FindpalabrasConLetra(List<string> palabras, char letra)
+    {
+        return palabras.Where(word => word.Contains(letra)).ToList();
+    }
+}
