10e141e4c007d1d2e78e0b5fa6c38c657658b616
GabrielaCode2104 <167550549+GabrielaCode2104@users.noreply.github.com>
2024-05-18 11:18:21 -0500

    8

---
+++ EJERCICIO01L/Ejercicio 8/Program.cs
@@ -0,0 +1,24 @@
+﻿// #8
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese las palabras separadas por comas:");
+        string input = Console.ReadLine();
+        List<string> palabras = input.Split(',').Select(x => x.Trim()).ToList();
+        List<string> palindromos = FindPalindromos(palabras);
+
+        Console.WriteLine("Palíndromos: " + string.Join(", ", palindromos));
+    }
+
+    static List<string> FindPalindromos(List<string> palabras)
+    {
+        return palabras.Where(IsPalindromo).ToList();
+    }
+
+    static bool IsPalindromo(string palabra)
+    {
+        string palabraInvertida = new string(palabra.Reverse().ToArray());
+        return palabra.Equals(palabraInvertida, StringComparison.OrdinalIgnoreCase);
+    }
+}
