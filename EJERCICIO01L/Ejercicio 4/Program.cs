dbd5526f3e81344c5e34020487934c86f4fa262e
GabrielaCode2104 <167550549+GabrielaCode2104@users.noreply.github.com>
2024-05-18 04:00:46 -0500

    4

---
+++ EJERCICIO01L/Ejercicio 4/Program.cs
@@ -0,0 +1,21 @@
+﻿// 4.Escriba una función que reciba dos conjuntos de números
+// y devuelva un conjunto con los números que están en ambos conjuntos. 
+class Program
+{
+    static void Main()
+    {
+        int[] conjunto1 = { 11, 3, 54, 20, 24 ,23 ,26 };
+        int[] conjunto2 = { 11, 5, 20, 70, 8, 3, 24, 4 };
+
+        var interseccionNumeros = GetCommonNumbers(conjunto1, conjunto2);
+
+        Console.WriteLine("Conjunto 1: " + string.Join(", ", conjunto1));
+        Console.WriteLine("Conjunto 2: " + string.Join(", ", conjunto2));
+        Console.WriteLine("Intersección: " + string.Join(", ", interseccionNumeros));
+    }
+
+    static IEnumerable<int> GetCommonNumbers(IEnumerable<int> conjunto1, IEnumerable<int> conjunto2)
+    {
+        return conjunto1.Intersect(conjunto2);
+    }
+}
