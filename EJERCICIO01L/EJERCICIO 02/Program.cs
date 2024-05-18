bd9cea7e142a00d6e4162b61014e1a766587b756
GabrielaCode2104 <167550549+GabrielaCode2104@users.noreply.github.com>
2024-05-18 03:50:37 -0500

    ejerc

--- EJERCICIO01L/EJERCICIO 02/Program.cs
+++ EJERCICIO01L/EJERCICIO 02/Program.cs
@@ -4,11 +4,13 @@ class Program
 {
     static void Main()
     {
-        string[] palabras = {"manzana", "plátano", "kiwi", "mango", "naranja", "pera", "uva" };
+        string[] palabras = {"manzana","plátano","kiwi","mango","naranja","pera","uva" };
         char letra = 'p';
+        Console.WriteLine("Lista original de palabras: " + string.Join(", ", palabras));
+        Console.WriteLine("Letra predeterminado con el que comienzan: " + letra);
         var empezarConLetra = GetWordsStartingWithLetter(palabras, letra);
 
-        Console.WriteLine(string.Join(" ", empezarConLetra));
+        Console.WriteLine("Palabras que inician con la letra predeterminada: " + string.Join(", ", empezarConLetra));
     }
 
     static IEnumerable<string> GetWordsStartingWithLetter(IEnumerable<string> palabras, char letra)
---
+++ EJERCICIO01L/EJERCICIO03/Program.cs
@@ -0,0 +1,22 @@
+﻿//3. 3.	Escriba una función que reciba un conjunto de números y
+//devuelva un conjunto con los números que son divisibles por un número determinado. 
+
+class Program
+{
+    static void Main()
+    {
+        int[] numeros1 = { 6,33,8,9,12,15,16,27,29,30,10,34,37,89,46,76,66 };
+        int divisor = 3;
+        Console.WriteLine("Lista original de números: " + string.Join(" ", numeros1));
+        Console.WriteLine("Número predeterminado (divisor): " + divisor);
+
+        var divisibleNumero = GetNumbersDivisibleByNumber(numeros1, divisor);
+        Console.WriteLine("Los número divisibles por el número predeterminado son: " + string.Join(" ", divisibleNumero));
+    }
+
+    static IEnumerable<int> GetNumbersDivisibleByNumber(IEnumerable<int> numeros1, int divisor)
+    {
+        return numeros1.Where(n => n % divisor == 0);
+    }
+}
+
