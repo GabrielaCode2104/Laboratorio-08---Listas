85340077a9106623ae5e2c2e13b829ebeaceb611
GabrielaCode2104 <167550549+GabrielaCode2104@users.noreply.github.com>
2024-05-18 16:39:25 -0500

    20

---
+++ EJERCICIO01L/Ejercicio 19/Program.cs
@@ -0,0 +1,18 @@
+﻿// #19
+class Aplicacion
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese los números separados por comas: ");
+        string entrada = Console.ReadLine();
+        List<int> listaDeNumeros = entrada.Split(',').Select(x => int.Parse(x.Trim())).ToList();
+
+        List<int> numerosOrdenados = EliminarDuplicadosOrdenar(listaDeNumeros);
+
+        Console.WriteLine("Números ordenados de menor a mayor sin duplicados: " + string.Join(", ", numerosOrdenados));
+    }
+    static List<int> EliminarDuplicadosOrdenar(List<int> listaDeNumeros)
+    {
+        return listaDeNumeros.Distinct().OrderBy(numero => numero).ToList();
+    }
+}
---
+++ EJERCICIO01L/Ejercicio 20/Program.cs
@@ -0,0 +1,22 @@
+﻿// #20
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese las palabras separadas por comas: ");
+        string input = Console.ReadLine();
+        List<string> palabras = input.Split(',').Select(x => x.Trim()).ToList();
+        Console.WriteLine("Ingrese la longitud deseada: ");
+        int longitud = Convert.ToInt32(Console.ReadLine());
+        List<string> palabrasFiltradas = FindPalindromosLongitud(palabras, longitud);
+
+        Console.WriteLine("Palabras palíndromos de longitud " + longitud + " ordenadas de menor a mayor: " + 
+            string.Join(", ", palabrasFiltradas));
+    }
+
+    static List<string> FindPalindromosLongitud(List<string> palabras, int longitud)
+    {
+        return palabras.Where(palabra => palabra.Length == longitud && palabra == 
+        new string(palabra.Reverse().ToArray())).OrderBy(palabra => palabra).ToList();
+    }
+}
